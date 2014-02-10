a284c326414bce5cb9782367599a41cf8ac7d20d
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-02-10 15:51:34 +0200

    Organised the code into functions.

--- myuniversity-courses.py
+++ myuniversity-courses.py
@@ -2,6 +2,26 @@ from selenium import webdriver
 from time import localtime
 import csv
 
+TARGET = 'http://myuniversity.gov.au/UndergraduateCourses'
+ROOT_XPATH = "//div[@class='myuni-small-cell-block']"
+LEAF_XPATH = './/span'
+
+def parse_page():
+	print 'Parsing web page ...'
+	results = browser.find_elements_by_xpath(ROOT_XPATH)
+	for result in results:
+		values = result.find_elements_by_xpath(LEAF_XPATH)
+		course = {
+			'Course Name': values[0].text,
+			'Cut-off ATAR': values[2].text,
+			'Duration': values[4].text,
+			'Award Type': values[5].text,
+			'Field of Education': values[6].text,
+			'Provider': values[7].text,
+			'Campus': values[8].text,
+		}
+		courses.append(course)
+
 def get_timestamp():
 	current_time = localtime()
 	return '-'.join(map(str, [
@@ -13,36 +33,32 @@ def get_timestamp():
 		current_time.tm_sec,
 	]))
 
+def create_csv():
+	column_names = [
+		'Course Name',
+		'Cut-off ATAR',
+		'Duration',
+		'Award Type',
+		'Field of Education',
+		'Provider',
+		'Campus',
+	]
+
+	file_name = 'courses-' + get_timestamp() + '.csv'
+	print 'Writing results to ', file_name, ' ...'
+
+	output_file = open(file_name, 'wb')
+	dict_writer = csv.DictWriter(output_file, column_names)
+	dict_writer.writer.writerow(column_names)
+	dict_writer.writerows(courses)
+
 print 'Opening web browser ...'
-
 browser = webdriver.Firefox()
-browser.get('http://myuniversity.gov.au/UndergraduateCourses')
-
-print 'Parsing web page ...'
+browser.get(TARGET)
 
 courses = []
-results = browser.find_elements_by_xpath("//div[@class='myuni-small-cell-block']")
-for result in results:
-	values = result.find_elements_by_xpath('.//span')
-	course = {
-		'Course Name': values[0].text,
-		'Cut-off ATAR': values[2].text,
-		'Duration': values[4].text,
-		'Award Type': values[5].text,
-		'Field of Education': values[6].text,
-		'Provider': values[7].text,
-		'Campus': values[8].text,
-	}
-	courses.append(course)
 
-print 'Writing results to courses.csv file ...'
-
-column_names = ['Course Name', 'Cut-off ATAR', 'Duration', 'Award Type', 'Field of Education', 'Provider', 'Campus']
-file_name = 'courses-' + get_timestamp() + '.csv'
-output_file = open(file_name, 'wb')
-
-dict_writer = csv.DictWriter(output_file, column_names)
-dict_writer.writer.writerow(column_names)
-dict_writer.writerows(courses)
+parse_page()
+create_csv()
 
 print 'Done.'
